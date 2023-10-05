d896149b80000d921d97aa3dfcc80060899dbc68
Your Name <thisarad582@gmail.com>
2023-10-05 14:46:28 +0530

    adding the new customer files

---
+++ src/components/customer/customerlandingpage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../common/header'
+import Footer from '../common/footer'
+import Customersearchbar from './customersearchbar'
+function Customerlandingpage(){
+    return(
+        <div>
+            <Header/>
+            <Customersearchbar/>
+            <Footer/>
+        </div>
+    )
+    
+}
+
+export default Customerlandingpage
