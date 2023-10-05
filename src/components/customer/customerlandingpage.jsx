77190bdd02223bf4076df7dc78ed5842b7b76594
Your Name <thisarad582@gmail.com>
2023-10-05 15:53:56 +0530

    more modifications

--- src/components/customer/customerlandingpage.jsx
+++ src/components/customer/customerlandingpage.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import Header from '../common/header'
 import Footer from '../common/footer'
 import Customersearchbar from './customersearchbar'
+import Customersearchresults from './customersearchresults'
+import Customershopsforyou from './customershopsforyou'
+
 function Customerlandingpage(){
     return(
         <div>
             <Header/>
             <Customersearchbar/>
+            <Customersearchresults/>
+            <Customershopsforyou/>
             <Footer/>
         </div>
     )
---
+++ src/components/customer/customershopsforyou.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import Galaxys9 from '../../resources/images/galaxys9.jpg';
+import Mobileshopvec from '../../resources/images/mobileshopvec.jpg';
+import Toysshopvec from '../../resources/images/toyshopvec.jpg';
+function Customershopsforyou(){
+    return(
+        <div style={{"margin-bottom":"200px","margin-top":"100px","margin-left":"50px"}}>
+            <div class="section">
+                    <h4>These shops are just for you</h4>
+            </div>
+            <div class ="row" >
+            <div class="card text-center" style={{"width": "18rem"}}>
+                
+                <div class="card-body">
+                    <h5 class="card-title">ID Phone store</h5>
+                    <img class="card-img-top" src= {Mobileshopvec} alt="mobilephone"/>
+                    <h6 class="card-text">we sell only high quality android phones</h6>
+                    <a href="#" class="btn btn-dark">see details</a>
+                </div>
+            </div>
+
+            <div class="card text-center" style={{"width": "18rem"}}>
+                
+                <div class="card-body">
+                    <h5 class="card-title">Shiva Phone store</h5>
+                    <img class="card-img-top" src={ Mobileshopvec} alt="mobilephone"/>
+                    <h6 class="card-text">We are well established sellers of mobile phones</h6>
+                    <a href="#" class="btn btn-dark">see details</a>
+                </div>
+            </div>
+
+            <div class="card text-center" style={{"width": "18rem"}}>
+                
+                <div class="card-body">
+                    <h5 class="card-title">TD Kids world</h5>
+                    <img class="card-img-top" src={ Toysshopvec} alt="mobilephone"/>
+                    <h6 class="card-text">Island's largest kids item collection</h6>
+                    <a href="#" class="btn btn-dark">see details</a>
+                </div>
+            </div>
+
+            <div class="card text-center" style={{"width": "18rem"}}>
+                
+                <div class="card-body">
+                    <h5 class="card-title">TD Kids world</h5>
+                    <img class="card-img-top" src={ Toysshopvec} alt="mobilephone"/>
+                    <h6 class="card-text">Island's largest kids item collection</h6>
+                    <a href="#" class="btn btn-dark">see details</a>
+                </div>
+            </div>
+
+            <div class="card" style={{"width": "18rem"}}>
+                
+                <div class="card-body">
+                    <h5 class="card-title">TD Kids world</h5>
+                    <img class="card-img-top" src={ Toysshopvec} alt="mobilephone"/>
+                    <h6 class="card-text">Island's largest kids item collection</h6>
+                    <a href="#" class="btn btn-dark">see details</a>
+                </div>
+            </div>
+
+        </div>
+        </div>
+        
+    )
+}
+
+export default Customershopsforyou 
